c2451f29a67c10b928e280d8872299f69b973e7c
Praveen Balijepalli <108292818+praveenbalijepalli@users.noreply.github.com>
2022-11-11 00:00:11 +0530

    Add files via upload

--- predict-test.py
+++ predict-test.py
@@ -11,7 +11,7 @@ url = "http://127.0.0.1:9696/predict"
  
     
 ### Test input 1
-test_data1 = test.sample(1, random_state=100)  
+test_data1 = test.sample(1, random_state=100) # Dropping label column because it should be the predicted output
 print("Test input data 1 with label to verify the predicted output: \n", test_data1)
 print("\n\n")
 
@@ -19,8 +19,9 @@ test_data1.drop("label", axis=1, inplace=True) # Dropping label column because i
 dict_test_data1 = json.loads(test_data1.to_json(orient='records', lines=True))
 
 response1 = requests.post(url, json=dict_test_data1).json()
-print(response1)
+print(json.dumps(response1,indent=2))
 print("\n\n\n")
+
  
 ### Test input 2
 test_data2 = test.sample(1, random_state=362) 
@@ -31,5 +32,5 @@ test_data2.drop("label", axis=1, inplace=True) # Dropping label column because i
 dict_test_data2 = json.loads(test_data2.to_json(orient='records', lines=True))
  
 response2 = requests.post(url, json=dict_test_data2).json()
-print(response2)
+print(json.dumps(response2,indent=2))
 print("\n\n\n")
