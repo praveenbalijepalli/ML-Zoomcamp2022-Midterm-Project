44824d471dbb337f84900390b2016b5e1bd36ef2
Praveen Balijepalli <108292818+praveenbalijepalli@users.noreply.github.com>
2022-11-10 23:48:52 +0530

    Update predict-test.py

--- predict-test.py
+++ predict-test.py
@@ -19,9 +19,8 @@ test_data1.drop("label", axis=1, inplace=True) # Dropping label column because i
 dict_test_data1 = json.loads(test_data1.to_json(orient='records', lines=True))
 
 response1 = requests.post(url, json=dict_test_data1).json()
-print(response1)
+print(json.dumps(response1, indent=2)
 print("\n\n\n")
-
  
 ### Test input 2
 test_data2 = test.sample(1, random_state=362) 
@@ -32,5 +31,5 @@ test_data2.drop("label", axis=1, inplace=True) # Dropping label column because i
 dict_test_data2 = json.loads(test_data2.to_json(orient='records', lines=True))
  
 response2 = requests.post(url, json=dict_test_data2).json()
-print(response2)
+print(json.dumps(response2, indent=2)
 print("\n\n\n")
